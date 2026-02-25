550cb490da9db3f0fe22c5e8fbb4a34c38963f8d
Emma <emmatalbot199@gmail.com>
2026-02-25 09:47:47 -0500

    Update comments for movement input clarity

--- StylusController.cs
+++ StylusController.cs
@@ -6,8 +6,8 @@ public class StylusCursorController : MonoBehaviour
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal"); // A/D or Left/Right
-        float z = Input.GetAxis("Vertical");   // W/S or Up/Down
+        float x = Input.GetAxis("Horizontal"); //Moves Left and Right (A and D)
+        float z = Input.GetAxis("Vertical");   // Moves up and down (W and S)
 
         Vector3 move = new Vector3(x, 0, z);
         transform.Translate(move * speed * Time.deltaTime);
